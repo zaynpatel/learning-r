9329c443ce1705ae5eed71aa7b7a1c7c3fe1a649
zaynpatel <65105835+zaynpatel@users.noreply.github.com>
2023-01-17 20:39:16 -0500

    Create describe.r

---
+++ day-one-hw/describe.r
@@ -0,0 +1,8 @@
+pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes, ggvis, httr, lubridate, plotly, rio, rmarkdown, shiny, stringr, tidyr)
+p_load(psych)
+
+p_help(psych)
+p_help(psych, web = F)
+
+describe(iris$Sepal.Length)
+describe(iris)
